97ad463767e4224c083595e1997c6efd3141053e
ahmadzughaer <ahmadzughaer@gmail.com>
2022-08-02 12:48:11 +0300

    private vs public members

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const button = document.querySelector('.button')
 const numberValue = document.querySelector('.counterText')
 
 class clickCounter {
+    #setCounter
     constructor() {
-        this.setCounter=0;
+        this.#setCounter=0;
     }
     getNumberOfClicks = () => {
-        console.log(this.setCounter)
+        console.log(this.#setCounter)
     }
     
     click = () => {
         button.addEventListener('click', () => {
-            numberValue.innerText = ++this.setCounter;
+            numberValue.innerText = ++this.#setCounter;
             this.getNumberOfClicks();
         })
     }
@@ -20,3 +21,27 @@ class clickCounter {
 
 const newCounter = new clickCounter()
 newCounter.click()
+console.log(newCounter.setCounter); // undefined 
+
+// private member
+class Something1 {
+    #property;
+    constructor(){
+      this.#property = 0;
+    }
+  }
+  
+  const instance1 = new Something1();
+  console.log(instance1.property);
+
+  
+// public member
+class Something2 {
+    property;
+    constructor(){
+      this.property = 0;
+    }
+  }
+  
+  const instance2 = new Something2();
+  console.log(instance2.property);
